65d05589a26d1abe21656e21f816b8a6d3f9918d
Jeriam <jeriamrogers@gmail.com>
2025-06-20 18:50:44 -0400

    push actual

--- lib/screens/mesero_page.dart
+++ lib/screens/mesero_page.dart
@@ -69,14 +69,27 @@ class MeseroPage extends StatelessWidget {
         automaticallyImplyLeading: false,
         title: const Text("Panel del Mesero"),
         backgroundColor: Colors.red,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout),
-            tooltip: "Cerrar sesión",
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-              Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+        actions:[
+          PopupMenuButton<String>(
+            icon: const Icon(Icons.settings),
+            onSelected: (value) async {
+              if (value == 'cerrar') {
+                await FirebaseAuth.instance.signOut();
+                Navigator.pushNamedAndRemoveUntil(context, '/', (_) => false);
+              } else if (value == 'cambiar') {
+                Navigator.pushNamed(context, '/cambiarClave');
+              }
             },
+            itemBuilder: (context) => [
+              const PopupMenuItem(
+                value: 'cambiar',
+                child: Text('Cambiar clave'),
+              ),
+              const PopupMenuItem(
+                value: 'cerrar',
+                child: Text('Cerrar sesión'),
+              ),
+            ],
           ),
         ],
       ),
